0b6705ddec210115d779ba4a97327337302136b5
DiegoMoreira98 <diego.moreira098@gmail.com>
2023-11-25 13:48:01 +0100

    changes

--- Assets/Scripts/Slot.cs
+++ Assets/Scripts/Slot.cs
@@ -13,14 +13,20 @@ public class Slot : MonoBehaviour
     public bool empty;
     public Sprite icon;
 
-    public Transform slotIconGameObject;
+    public Image slotIconImage;
 
     private void Start()
     {
-        slotIconGameObject = transform.GetChild(0);
+        slotIconImage = transform.GetChild(0).GetComponent<Image>(); // Asigno directamente el componente Image.
     }
+
     public void UpdateSlot()
     {
-        slotIconGameObject.GetComponent<Image>().sprite = icon;
+        slotIconImage.sprite = icon; // Accedo directamente al componente Image.
+
+        if (slotIconImage == null)
+        {
+            Debug.LogError("Image component not found in child. Make sure the child has an Image component attached.");
+        }
     }
 }
