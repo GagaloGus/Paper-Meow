619a390cd22c236aba353818e346f16c92faee92
DiegoMoreira98 <diego.moreira098@gmail.com>
2023-12-18 23:44:30 +0100

    Dropdown Resolucion y limite fps

--- Assets/Scripts/FPSController.cs
+++ Assets/Scripts/FPSController.cs
@@ -1,8 +1,8 @@
-using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class FPSCounter : MonoBehaviour
 {
@@ -10,11 +10,49 @@ public class FPSCounter : MonoBehaviour
     GUIStyle style = new GUIStyle();
     public int targetFPS;
     public int vsyncValue = 1;
+    public TMPro.TMP_Dropdown resolutionDropdown;
+    public TMPro.TMP_Dropdown fpsLimitDropdown;
 
     private void Awake()
     {
         Application.targetFrameRate = targetFPS;
         QualitySettings.vSyncCount = vsyncValue;
+    }
+
+    private void Start()
+    {
+        // Obtener las resoluciones disponibles
+        Resolution[] resolutions = Screen.resolutions;
+
+        // Limpiar opciones existentes en el dropdown
+        resolutionDropdown.ClearOptions();
+
+        // Lista para almacenar las opciones de texto
+        List<string> options = new List<string>();
+
+        // Agregar las resoluciones al dropdown
+        foreach (Resolution res in resolutions)
+        {
+            string optionText = $"{res.width} x {res.height} {res.refreshRateRatio}Hz";
+            options.Add(optionText);
+        }
+
+        // Agregar opciones al dropdown
+        resolutionDropdown.AddOptions(options);
+
+        // Configurar el método que se ejecutará cuando cambie la resolución
+        resolutionDropdown.onValueChanged.AddListener(ChangeResolution);
+
+        // Limpiar opciones existentes en el dropdown
+        fpsLimitDropdown.ClearOptions();
+
+        // Configurar opciones de límites de FPS
+        List<string> fpsLimitOptions = new List<string> { "30", "60", "120", "144", "165", "Unlimited" };
+        fpsLimitDropdown.AddOptions(fpsLimitOptions);
+
+        // Configurar el método que se ejecutará cuando cambie el límite de FPS
+        fpsLimitDropdown.onValueChanged.AddListener(ChangeFPSLimit);
+
     }
     void Update()
     {
@@ -36,9 +74,18 @@ public class FPSCounter : MonoBehaviour
         GUI.Label(new Rect(10, 10, 100, 20), text, style);
     }
 
-    public void ChangeFPSLimit(int num)
+    public void ChangeFPSLimit(int index)
     {
-        targetFPS = (num);
+        string selectedOption = fpsLimitDropdown.options[index].text;
+
+        if (selectedOption == "Unlimited")
+        {
+            targetFPS = -1; // Valor negativo para FPS ilimitado
+        }
+        else
+        {
+            targetFPS = int.Parse(selectedOption);
+        }
     }
 
     public void ToggleVSync()
@@ -48,5 +95,17 @@ public class FPSCounter : MonoBehaviour
         QualitySettings.vSyncCount = vsyncValue;
 
         Debug.Log($"VSync {(vsyncValue == 0 ? "desactivado" : "activado")}");
+    }
+    public void ChangeResolution(int index)
+    {
+        // Obtener las resoluciones disponibles
+        Resolution[] resolutions = Screen.resolutions;
+
+        // Aplicar la resolución seleccionada
+        if (index >= 0 && index < resolutions.Length)
+        {
+            Resolution selectedResolution = resolutions[index];
+            Screen.SetResolution(selectedResolution.width, selectedResolution.height, Screen.fullScreen);
+        }
     }
 }
