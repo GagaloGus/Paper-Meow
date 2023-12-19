8f4cb4c5ead1c0fa27473b552c845f3f71f3a287
DiegoMoreira98 <diego.moreira098@gmail.com>
2023-12-19 19:00:52 +0100

    minor changes

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,14 @@
+using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public CinemachineFreeLook cinemachine;
+
+    private void Start()
+    {
+        cinemachine.m_Lens.FieldOfView = 17;
+    }
+}
