d6a68a35ba0ad5e5428e94cd4ee88702acbab45a
GagaloGus <gabriel.mendez.sas@gmail.com>
2024-01-30 13:59:40 +0100

    martillo spin a mazazo

--- Assets/Scripts/Extra/AnimationEvents.cs
+++ Assets/Scripts/Extra/AnimationEvents.cs
@@ -4,17 +4,24 @@ using UnityEngine;
 
 public class AnimationEvents : MonoBehaviour
 {
+    GameObject player;
+
+    private void Start()
+    {
+        player = FindObjectOfType<SkoController>().gameObject;
+    }
+
     //llamado desde las animaciones de ataque, solo sirve para el player
     public void NotAttacking()
     {
-        SkoController cont = FindObjectOfType<SkoController>();
+        SkoController cont = player.GetComponent<SkoController>();
         cont.player_isAttacking = false;
         cont.currentAttackNumber = 0;
     }
 
     public void FollowUpAttack()
     {
-        SkoController cont = FindObjectOfType<SkoController>();
+        SkoController cont = player.GetComponent<SkoController>();
         cont.canAttackAgain = true;
     }
 }
