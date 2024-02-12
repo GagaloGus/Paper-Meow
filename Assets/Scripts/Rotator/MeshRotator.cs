e877e684aebc2af5318c86bedec8b294acfaf185
Rubén Llanes Rioja <rubenllanes47@gmail.com>
2024-02-12 19:46:30 +0100

    Map 1.4

---
+++ Assets/Scripts/Rotator/MeshRotator.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+public class MeshRotator : MonoBehaviour
+{
+    public MeshFilter meshFilter;
+    public void ApplyRotationToVertices()
+    {
+        Mesh mesh = meshFilter.sharedMesh;
+        Vector3[] vertices = mesh.vertices;
+        for (int v = 0; v < vertices.Length; v++) vertices[v] = meshFilter.transform.TransformPoint(vertices[v]);
+        meshFilter.transform.rotation = Quaternion.identity;
+        for (int v = 0; v < vertices.Length; v++) vertices[v] = meshFilter.transform.InverseTransformPoint(vertices[v]);
+        meshFilter.sharedMesh.vertices = vertices;
+        meshFilter.sharedMesh.RecalculateBounds();
+    }
+}
