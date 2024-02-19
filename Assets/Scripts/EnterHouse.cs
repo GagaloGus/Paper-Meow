fb1909b889ff909fa22bfa488ee05b40bdf89bdf
Rino4700 <rubenllanes47@gmail.com>
2024-02-19 12:43:09 +0100

    Barriers

--- Assets/Scripts/EnterHouse.cs
+++ Assets/Scripts/EnterHouse.cs
@@ -6,14 +6,17 @@ using UnityEngine;
 
 public class EnterHouse : MonoBehaviour
 {
-    public GameObject DoorTP, LastDoorTP, LDTPparent;
-    private GameObject Player;
+    public GameObject DoorTP, LastDoorTP;
+    private GameObject Player; /*LDTPparent, LDTPSons, TPparent, TPsons*/
     private bool InRange = false;
     public bool isInside;
+    //private GameObject[] OutDoors, InDoors;
     void Start()
     {
         Player = FindAnyObjectByType<SkoController>().gameObject;
         isInside = gameObject.name.Contains("Enter");
+        //OutDoors = new GameObject[3];
+        //InDoors = new GameObject[3];
     }
 
     void Update()
@@ -21,7 +24,17 @@ public class EnterHouse : MonoBehaviour
         if(InRange && Input.GetKeyDown(KeyCode.E) && isInside)
         {
             LastDoorTP = gameObject;
-            LDTPparent = LastDoorTP.transform.parent.gameObject;
+            /*LDTPparent = LastDoorTP.transform.parent.gameObject;
+            LDTPSons = LDTPparent.GetComponentInChildren<EnterHouse>().gameObject;
+            LDTPSons.GetComponent<EnterHouse>().LastDoorTP = LDTPSons.gameObject;
+            TPparent = DoorTP.transform.parent.gameObject;
+            TPsons = TPparent.GetComponentInChildren<EnterHouse>().gameObject;
+            for (int i = 0; i < OutDoors.Length; i++)
+            {
+                OutDoors[i] = TPsons;
+            }
+            InDoors[i] = LDTPSons;
+            OutDoors[i].GetComponent<EnterHouse>().DoorTP = InDoors[i].GetComponent<EnterHouse>().LastDoorTP;*/
             DoorTP.gameObject.GetComponent<EnterHouse>().DoorTP = LastDoorTP;
             Player.transform.position = DoorTP.transform.position;
             InRange = false;
